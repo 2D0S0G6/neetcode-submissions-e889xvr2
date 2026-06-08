0741f68d216da2e05d688020c62068ccc65a0105
Deepak S G <142807455+2D0S0G6@users.noreply.github.com>
2026-06-08 12:36:09 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,10 @@
+
+class Solution:
+    def groupAnagrams(self, strs):
+        groups = defaultdict(list)
+
+        for s in strs:
+            key = ''.join(sorted(s))
+            groups[key].append(s)
+
+        return list(groups.values())
